af7bb84d8bd1aa68a87393cc028e0c4bfc441736
GMSuper <2057847750@qq.com>
2018-01-29 20:10:56 +0800

    配置项目

---
+++ Parking/Parking/Classes/Other/PIToolHeader.h
@@ -0,0 +1,21 @@
+//
+//  PIToolHeader.h
+//  Parking
+//
+//  Created by apple on 2018/1/29.
+//  Copyright © 2018年 GM. All rights reserved.
+//
+
+#ifndef PIToolHeader_h
+#define PIToolHeader_h
+
+#import <AFNetworking.h>
+#import <MJRefresh.h>
+#import <MJExtension.h>
+#import <MBProgressHUD.h>
+#import <IQKeyboardManager.h>
+#import <UIImageView+WebCache.h>
+#import "UIView+PIExtension.h"
+
+
+#endif /* PIToolHeader_h */
